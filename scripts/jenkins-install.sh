55966e6a6572a5295644dc2e8634c3283c25b857
Justin Domingus <domingusj@gmail.com>
2016-06-26 16:37:08 -0700

    rails new and update jenkins script

--- scripts/jenkins-install.sh
+++ scripts/jenkins-install.sh
@@ -4,3 +4,5 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins && sudo service jenkins restart
+sudo apt-get install -y git
+echo "DONE - now install and configre mailutils"
