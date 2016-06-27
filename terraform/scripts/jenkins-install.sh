292c4f5dc83859279ccd066531164ee6f2b3be44
Justin Domingus <domingusj@gmail.com>
2016-06-26 19:20:19 -0700

    add docker to jenkins install

--- terraform/scripts/jenkins-install.sh
+++ terraform/scripts/jenkins-install.sh
@@ -4,5 +4,7 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins && sudo service jenkins restart
-sudo apt-get install -y git ruby
-echo "DONE - now install and configre mailutils"
+sudo apt-get install -y git ruby python-pip
+wget -qO- https://get.docker.com/ | sh
+sudo pip install docker-compose
+echo "Done provisioning Jenkins"
